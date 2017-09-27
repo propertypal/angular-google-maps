521a1ef2c72b7606ba09409374c4da548a47ebb9
Paul Anthony <paul.anthony@propertypal.com>
2017-09-27 08:51:55 +0100

    add child communicator component

---
+++ packages/core/directives/agm-child.ts
@@ -0,0 +1,18 @@
+import { Directive, EventEmitter, OnInit, Output } from '@angular/core';
+import { GoogleMapsAPIWrapper } from './../services/google-maps-api-wrapper';
+
+
+@Directive({
+  selector: 'agm-child'
+})
+export class AgmChildComponent implements OnInit {
+  @Output() onMapLoad: EventEmitter<{}> = new EventEmitter<{}>();
+
+  constructor(public gMaps: GoogleMapsAPIWrapper) {}
+
+  ngOnInit() {
+    this.gMaps.getNativeMap().then((map) => {
+      this.onMapLoad.emit(map);
+    });
+  }
+}
